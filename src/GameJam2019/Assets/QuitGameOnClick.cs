a59587be3b8b6e8c434f75d2bd937bc68a367113
Lance Erickson <lancee@iqmetrix.com>
2019-01-26 16:34:40 -0600

    - Quit game functionality - Proof of concept for pause screen

---
+++ src/GameJam2019/Assets/QuitGameOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class QuitGameOnClick : MonoBehaviour
+{
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ src/GameJam2019/Assets/ToggleSetActiveOnEscKey.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ToggleSetActiveOnEscKey : MonoBehaviour
+{
+	public GameObject GameObjectToSet;
+
+    void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+			GameObjectToSet.SetActive(!GameObjectToSet.activeSelf);
+	}
+}

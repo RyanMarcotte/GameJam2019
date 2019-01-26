798a57c4855630237d23a9d95bc2f878c2728e98
Ryan Marcotte <RyanM@iqmetrix.com>
2019-01-26 14:15:55 -0600

    added LightMap object

---
+++ src/GameJam2019/Assets/LightMapController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMapController : MonoBehaviour
+{
+	public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	// Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnGUI()
+	{
+
+	}
+}
--- src/GameJam2019/Assets/TerrainGenerator.cs
+++ src/GameJam2019/Assets/TerrainGenerator.cs
@@ -10,6 +10,7 @@ public class TerrainGenerator : MonoBehaviour
 
 	public Tilemap Floor;
 	public Tilemap Obstacles;
+	public GameObject LightMap;
 
 	// Start is called before the first frame update
 	void Start()

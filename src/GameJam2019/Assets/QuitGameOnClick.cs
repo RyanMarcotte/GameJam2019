6a43efe8b82c3d0e9bcf781ec89f872eb7c2bebf
Lance Erickson <lancee@iqmetrix.com>
2019-01-26 16:45:47 -0600

    - Editor quit handling

--- src/GameJam2019/Assets/QuitGameOnClick.cs
+++ src/GameJam2019/Assets/QuitGameOnClick.cs
@@ -7,6 +7,10 @@ public class QuitGameOnClick : MonoBehaviour
 {
 	public void QuitGame()
 	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
 		Application.Quit();
+#endif
 	}
 }

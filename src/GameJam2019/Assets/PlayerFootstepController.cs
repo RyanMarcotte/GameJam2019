f95399439243596d00504711387f7252760e2fe2
Nicholas Sutter <gastlyTree@gmail.com>
2019-01-26 16:55:54 -0600

    Added the noise item prefab. Added footsteps to the player. --Nicholas Sutter

---
+++ src/GameJam2019/Assets/PlayerFootstepController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepController : MonoBehaviour
+{
+    public AudioClip walkSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("PlaySound", 0.0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaySound()
+    {
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(walkSound);
+        }
+    }
+}

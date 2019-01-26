faa0dcaff5df38cc53e2b9796b2db0a8cf36f469
Lance Erickson <lancee@iqmetrix.com>
2019-01-26 17:00:10 -0600

    - Keyboard control for menus

---
+++ src/GameJam2019/Assets/SelectOnInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+	public EventSystem EventSystem;
+	public GameObject SelectedObject;
+
+	private bool IsButtonSelected;
+
+	void Update () 
+	{
+		if (Input.GetAxisRaw("Vertical") == 0 || IsButtonSelected) return;
+
+		EventSystem.SetSelectedGameObject(SelectedObject);
+		IsButtonSelected = true;
+	}
+
+	private void OnDisable() 
+		=> IsButtonSelected = false;
+}

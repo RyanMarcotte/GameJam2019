faa592d12630f6ca584b6e26261d4acf99943d79
Ryan Marcotte <RyanM@iqmetrix.com>
2019-01-25 21:36:48 -0600

    added player test scene; player moves around the map according to input

---
+++ src/GameJam2019/Assets/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Extensions;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+	private Rigidbody2D _body;
+	private float _horizontal;
+	private float _vertical;
+	private float _moveLimiter = 0.7f;
+	
+	public float RunSpeed = 5;
+
+	private void Start()
+	{
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		_horizontal = Input.GetAxisRaw("Horizontal");
+		_vertical = Input.GetAxisRaw("Vertical");
+	}
+
+	private void FixedUpdate()
+	{
+		_body.velocity = new Vector2(_horizontal, _vertical).ToNormalizedVector2() * RunSpeed;
+	}
+}

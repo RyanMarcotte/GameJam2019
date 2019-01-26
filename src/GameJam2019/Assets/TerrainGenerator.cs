028d6f8b445df3a511a7809aade7375db2507e14
Lance Erickson <lancee@iqmetrix.com>
2019-01-26 12:54:46 -0600

    -  Generate simple terrain from arbitrary b/w bitmap

--- src/GameJam2019/Assets/TerrainGenerator.cs
+++ src/GameJam2019/Assets/TerrainGenerator.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class TerrainGenerator : MonoBehaviour
 {
@@ -9,20 +11,57 @@ public class TerrainGenerator : MonoBehaviour
 	public Tilemap Floor;
 	public Tilemap Obstacles;
 
-	private int length = 64;
-	private int width = 64;
-
 	// Start is called before the first frame update
 	void Start()
 	{
-		var random = new System.Random();
-		for (int i = GetLowerBound(length); i < GetUpperBound(length); i++)
+		var bitmapReader = new BitmapReader();
+		var levelMap = bitmapReader.Read();
+
+		var tileXIndex = 0;
+		var tileYIndex = 0;
+
+		if (levelMap == null || levelMap.GetLength(0) == 0 || levelMap.GetLength(1) == 0)
+		{
+			GenerateRandomMap(64, 64);
+			return;
+		}
+
+		int height = levelMap.GetLength(0);
+		int width = levelMap.GetLength(1);
+
+		for (int i = GetLowerBound(height); i < GetUpperBound(height); i++)
 		{
 			for (int j = GetLowerBound(width); j < GetUpperBound(width); j++)
 			{
-				if (i == GetLowerBound(length)
-					|| i == GetUpperBound(length) - 1
-					|| j == GetUpperBound(width) - 1	
+				var tile = levelMap[tileYIndex, tileXIndex];
+				if (i == GetLowerBound(height)
+					|| i == GetUpperBound(height) - 1
+					|| j == GetUpperBound(width) - 1
+					|| j == GetLowerBound(width))
+					Obstacles.SetTile(new Vector3Int(i, j, -1), ObstacleTile);
+				if (tile == TileType.Ground)
+					Floor.SetTile(new Vector3Int(i, j, 0), GroundTile);
+				if (tile == TileType.Obstacle)
+					Obstacles.SetTile(new Vector3Int(i, j, -1), ObstacleTile);
+
+				tileYIndex++;
+			}
+
+			tileXIndex++;
+			tileYIndex = 0;
+		}
+	}
+
+	private void GenerateRandomMap(int height, int width)
+	{
+		var random = new System.Random();
+		for (int i = GetLowerBound(height); i < GetUpperBound(height); i++)
+		{
+			for (int j = GetLowerBound(width); j < GetUpperBound(width); j++)
+			{
+				if (i == GetLowerBound(height)
+					|| i == GetUpperBound(height) - 1
+					|| j == GetUpperBound(width) - 1
 					|| j == GetLowerBound(width))
 					Obstacles.SetTile(new Vector3Int(i, j, -1), ObstacleTile);
 				Floor.SetTile(new Vector3Int(i, j, 0), GroundTile);

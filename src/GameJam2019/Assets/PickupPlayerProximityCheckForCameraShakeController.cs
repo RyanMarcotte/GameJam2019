9bb2e90a568d1f6ec6d00483c11d6d0e7366047a
Ryan Marcotte <RyanM@iqmetrix.com>
2019-01-27 14:13:49 -0600

    fix shaking

--- src/GameJam2019/Assets/PickupPlayerProximityCheckForCameraShakeController.cs
+++ src/GameJam2019/Assets/PickupPlayerProximityCheckForCameraShakeController.cs
@@ -11,12 +11,10 @@ public class PickupPlayerProximityCheckForCameraShakeController : MonoBehaviour
 	public GameObject Player;
 	public GameObject Camera;
 
-	private static IDictionary<Vector3, float> _magnitudes;
-
-    // Start is called before the first frame update
+	// Start is called before the first frame update
     void Start()
     {
-        _magnitudes = new Dictionary<Vector3, float>();
+		
     }
 
     // Update is called once per frame
@@ -27,15 +25,11 @@ public class PickupPlayerProximityCheckForCameraShakeController : MonoBehaviour
 		    return;
 
 		var distance = (transform.position - Player.transform.position).magnitude;
-		if (!_magnitudes.ContainsKey(transform.position))
-			_magnitudes.Add(transform.position, 0);
-		_magnitudes[transform.position] = distance < MAXIMUM_DISTANCE ? (MAXIMUM_DISTANCE - distance) : 0f;
-		
-	    component.Magnitude = _magnitudes.Values.Sum();
+	    component.AddShake(distance < MAXIMUM_DISTANCE ? (MAXIMUM_DISTANCE - distance) : 0f);
     }
 
 	void OnDestroy()
 	{
-		_magnitudes.Remove(transform.position);
+		
 	}
 }
